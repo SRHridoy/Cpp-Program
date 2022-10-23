7dcd9d388573a8fc19bf63f79b97a1bb42dcd55a
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-10-23 23:39:44 +0600

    Added some previsious unsloved

--- CodeForces/Target 1-50/23_A. Nearly Lucky Number.cpp
+++ CodeForces/Target 1-50/23_A. Nearly Lucky Number.cpp
@@ -1,50 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    long long n;
+int main(){
+    long long int n;
     cin >> n;
 
-    long long last = 0;
-    int count = 0, flag = 0;
-    long long digit = log(10 * (n + 1));
-    for (int i = 0; i < digit; i++)
+    int reminder = 0, count = 0;
+    while (n)
     {
-        // cout << n << "-->" << last << "-->" <<count << "-->" <<flag << endl;
-        last = n % 10;
-        // cout << last;
-        if (last == 4 || last == 7)
+        reminder = n % 10;
+        n/=10;
+        if (reminder == 4 || reminder == 7)
         {
-            n/=10;
             count++;
-            flag = 1;
-        }
-        else
-        {
-            if (count == 7 || count == 4)
-            {
-                flag = 1;
-                 n/=10;
-            }
-            else
-            {
-                flag = 0;
-                 n/=10;
-            }
-            n/=10;
         }
         
     }
-    if ((count == 4 || count == 7) && flag == 1)
+    if (count == 4 || count == 7)
     {
         cout << "YES";
     }
-    else 
-    {
+    else
         cout << "NO";
-    }
-    // cout <<endl <<  n << "-->" << last << "-->" <<count << "-->" <<flag << endl;
-
+    
+    
     return 0;
 }
