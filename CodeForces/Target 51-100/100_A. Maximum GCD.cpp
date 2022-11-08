02a0e700ecb59e1ecebd51df3db7a461530a1d9c
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-11-09 01:57:41 +0600

    Reached my Second goal! Completed 100 Problems on CODEFORCES

---
+++ CodeForces/Target 51-100/100_A. Maximum GCD.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << n/2 << "\n";
+    }
+    
+
+
+    return 0;
+}
---
+++ CodeForces/Target 51-100/99_A. Again Twenty Five!.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    ll n;
+    cin >> n;
+    cout << "25\n";
+    
+    return 0;
+}
