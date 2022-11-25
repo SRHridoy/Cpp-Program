cd775c197385ce6eab05f9b668ee3425e84fd883
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-11-26 04:06:31 +0600

    Such a Productive day--> Completed my daily CodeForces Target...

---
+++ CodeForces/Target 100 -  200/151_A_Plus_One_on_the_Subset.cpp
@@ -0,0 +1,46 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define fo(i, n)          for(int i = 0; i <n; i++)
+using namespace std;
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int>v;
+    fo(i,n)
+    {
+        int x; cin >> x; 
+        v.pb(x);
+    }
+    cout << *max_element(v.begin(),v.end()) - *min_element(v.begin(),v.end()) << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    // t = 1;
+    //int cs = 1;
+    cin >> t;
+    while (t--)
+    {
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
---
+++ CodeForces/Target 100 -  200/153_A_Choosing_Teams.cpp
@@ -0,0 +1,52 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define fo(i, n)          for(int i = 0; i <n; i++)
+using namespace std;
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+    vector<int>v;
+    fo(i,n)
+    {
+        int x; cin >> x;
+        v.pb(x);
+    }
+
+    int cnt = 0;
+    fo(i,n)
+        if(5-v[i]>=k)
+            cnt++;
+    cout << cnt/3 <<endl;
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    t = 1;
+    //int cs = 1;
+    //cin >> t;
+    while (t--)
+    {
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
