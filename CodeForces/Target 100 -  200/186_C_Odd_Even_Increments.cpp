f67e4946eb6fdb5d2e844bd80e3f3353bc933fd5
Md Sohanur Rahman Hridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-07 13:03:49 +0600

    Completed my daily Code-Forces Taget...

---
+++ CodeForces/Target 100 -  200/186_C_Odd_Even_Increments.cpp
@@ -0,0 +1,48 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define fo(i, n)          for(int i = 0; i < n; i++)
+#define Fo(i,k,n)         for(int i = k;k<n?i<n:i>n;k<n?i++:i--)
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector<int>v;
+    fo(i,n){
+        int x; cin >> x;
+        v.pb(x);
+    } bool hobe_ki = true;
+    fo(i,n){
+        if(i-2>-1 and v[i] % 2!= v[i-2]%2){
+            hobe_ki = false;
+            break;
+        }
+    }hobe_ki?cout << "YES" << endl:cout <<"NO" << endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    //t = 1;
+    //int cs = 1;
+    cin >> t;
+    while (t--){
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
