5f17db6205c01b281d516d5a141834e29e738da5
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-27 22:26:25 +0600

    Completed My daily Code-Forces Target...

---
+++ CodeForces/Target 201-  500/201 - 250/229_A_Joey_Takes_Money.cpp
@@ -0,0 +1,47 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define all(v)            v.begin(),v.end()
+#define fo(i, n)          for(int i = 0; i < n; i++)
+#define Fo(i,k,n)         for(int i = k;k<n?i<n:i>n;k<n?i++:i--)
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    int mul = 1;
+    vector<int>v(n);
+    fo(i,n){
+        cin >> v[i];
+        mul*=v[i];
+    } 
+mul+=(n-1);
+    cout << mul*2022 << endl;
+    
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    //t = 1;
+    //int cs = 1;
+    cin >> t;
+    while (t--){
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
