d680d5d5710c38d4b6011cefb548743ee56eab54
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2022-12-28 10:35:36 +0600

    Completed my daily code forcess Target...

---
+++ CodeForces/Target 201-  500/201 - 250/232_A_Phoenix_and_Balance.cpp
@@ -0,0 +1,48 @@
+//       *Author: Md Sohanur Rahman Hridoy* ​
+
+//In the name of ALLAH, the Most Gracious, the Most Merciful...
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+
+#include"bits/stdc++.h"
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define endl              '\n'
+#define all(v)            v.begin(),v.end()
+#define fo(i, n)          for(int i = 0; i < n; i++)
+#define Fo(i,k,n)         for(int i = k;k<n?i<n:i>n;k<n?i++:i--)
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    //Putting largest coin in first pile
+    int sum1 = (1<<n), sum2 = 0;
+    //Putting (n/2)-1 smallest coins in the first pile
+    Fo(i,1,n/2){
+        sum1+=(1<<i);
+    }
+    //Putting remainig n/2 coins in second pile
+    Fo(i,n/2,n){
+        sum2+=(1<<i);
+    }cout << sum1 - sum2 << endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    //t = 1;
+    //int cs = 1;
+    cin >> t;
+    while (t--){
+        //cout << "Case-#" << cs <<": ";
+        solve();
+        //cs++;
+    }
+}
