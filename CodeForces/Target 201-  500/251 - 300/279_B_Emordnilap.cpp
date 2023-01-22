9eaac1550242866bef0c3cabfe1304a83a16f1a0
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-01-23 00:40:20 +0600

    Completed my daily Code- forces target...

--- CodeForces/Target 201-  500/251 - 300/279_B_Emordnilap.cpp
+++ CodeForces/Target 201-  500/251 - 300/279_B_Emordnilap.cpp
@@ -10,16 +10,33 @@
 #define pb                push_back
 #define ppb               pop_back
 #define pf                push_front
-#define ppf               pop_front-
+#define ppf               pop_front
 #define endl              '\n'
 #define all(v)            v.begin(),v.end()
 #define fo(i, n)          for(int i = 0; i < n; i++)
 #define Fo(i,k,n)         for(int i = k;k<n?i<n:i>n;k<n?i++:i--)
+#define MOD 1000000007
 using namespace std;
 
 void solve(){
-    int n; cin >> n;
-
+    int n;
+	cin >> n;
+	if (n == 1) {
+		cout << 0 << endl;
+		return;
+	}
+	
+	int f = 1;
+	for (int i=1; i<=n; i++) {
+		f *= i;
+		f %= MOD;
+	}
+	
+	int x = f;
+	x *= ((n * (n - 1))) % MOD;
+	x %= MOD;
+	
+	cout << x << endl;
 }
 
 int32_t main(){
